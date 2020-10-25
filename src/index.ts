e8d2849b9bcd04849174c68a7d1589603f27a735
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-25 08:38:36 -0300

    feat(index): login to Discord and resolve mute role

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import fs from "fs"
 import YAML from "yaml"
 import chalk from "chalk"
 import logger from "./logger"
-import { Schema } from "joi"
+import Discord from "discord.js"
 import normalizeConfig from "./util/normalizeConfig"
 import stringifyObject from "./util/stringifyObject"
 
+import { Schema } from "joi"
 import Module from "./modules/Module"
 const modules = {}
 
-const loadedModules: any = {}
+const loadedConfig: { muteRole?: Discord.Role; modules: any } = { modules: {} }
 
 async function main() {
     logger.debug("Loading config...")
@@ -32,6 +33,10 @@ async function main() {
     config = normalizeConfig(config)
     if (typeof config.modules !== "object")
         logger.warn("Module configuration missing; all modules are disabled.")
+    if (!config.token) {
+        logger.error("Discord token missing in config.")
+        process.exit(0)
+    }
 
     for (const module in config.modules) {
         if (!modules[module]) {
@@ -48,13 +53,40 @@ async function main() {
             logger.error(`While validating config for "${module}" module: ${message}.`)
             process.exit(0)
         }
-        loadedModules[module] = value
+        loadedConfig.modules[module] = value
 
         logger.info(
             `Loaded module "${module}" with config ${chalk.yellowBright(
                 stringifyObject(value)
             )}.`
         )
+    }
+
+    const before = Date.now()
+    const client = new Discord.Client()
+    await client.login(config.token).catch((error: Error) => {
+        logger.error(`While logging in to Discord: ${error.message}`)
+        process.exit(0)
+    })
+    logger.info(`Logged in to Discord in ${Date.now() - before}ms.`)
+
+    if (config.muteRole) {
+        if (client.guilds.cache.size > 1)
+            logger.warn("Bot is in multiple guilds, mute role will not be resolved.")
+        else if (client.guilds.cache.size === 0)
+            logger.warn("Bot is in no guilds, mute role will not be resolved.")
+        else {
+            const guild = client.guilds.cache.first()
+            const role = await guild.roles.fetch(config.muteRole, true).catch(() => null)
+
+            if (role instanceof Discord.Role) {
+                const color = chalk.hex(role.hexColor)
+                logger.info(`Resolved mute role ${color(role.name)}.`)
+                loadedConfig.muteRole = role
+            } else {
+                logger.warn(`Could not resolve mute role with ID "${config.muteRole}".`)
+            }
+        }
     }
 }
 
