25ce9dd888d7b27e9d4a93a0be61208e41777454
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-29 21:43:38 -0300

    feat(emptyMessages): add a few characters

--- src/modules/emptyMessages.ts
+++ src/modules/emptyMessages.ts
@@ -86,6 +86,7 @@ function astToText(content: Token[], type?: string): string {
 const characters = [
     "\u0020", // Space
     "\u00A0", // No-Break Space
+    "\u17B5", // Khmer Vowel Inherent Aa
     "\u2000", // En Quad
     "\u2001", // Em Quad
     "\u2002", // En Space
@@ -97,15 +98,21 @@ const characters = [
     "\u2008", // Punctuation Space
     "\u2009", // Thin Space
     "\u200A", // Hair Space
-    "\u2028", // Line Separator
-    "\u205F", // Medium Mathematical Space
-    "\u3000", // Ideographic Space
     "\u200B", // Zero Width Space
-    "\u2800", // Braille Pattern Blank
-    "\u3164", // Hangul Filler
-    "\u17B5", // Khmer Vowel Inherent Aa
+    "\u200C", // Zero Width Non-Joiner
+    "\u200D", // Zero Width Joiner
     "\u200E", // Left-to-Right Mark
-    "\r",
-    "\n",
-    "\t"
+    "\u200F", // Right-to-Left Mark
+    "\u2028", // Line Separator
+    "\u2029", // Paragraph Separator
+    "\u202F", // Narrow No-Break Space
+    "\u205F", // Medium Mathematical Space
+    "\u2800", // Braille Pattern Blank
+    "\u3000", // Ideographic Space
+    "\u3164", // Hangul Filler
+    "\uFEFF", // Zero Width No-Break Space
+    "\r", // Carriage Return
+    "\n", // Newline
+    "\t", // Tab
+    "\f" // Form Feed
 ]
