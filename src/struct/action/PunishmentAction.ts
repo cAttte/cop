9e2407c789291fbd7bcf36e75bc57f2b27312965
cAttte <26514199+cAttte@users.noreply.github.com>
2020-11-03 00:52:56 -0300

    feat(PunishmentAction): implement formatSuccess(), formatError()

--- src/struct/action/PunishmentAction.ts
+++ src/struct/action/PunishmentAction.ts
@@ -1,9 +1,11 @@
+import chalk from "chalk"
 import Discord from "discord.js"
 import Action, { ActionProperties } from "./Action"
 
 export default class PunishmentAction extends Action implements PunishmentProperties {
     type: "mute" | "kick" | "ban"
     length?: number
+    target: Discord.GuildMember
 
     constructor(properties: ActionProperties & PunishmentProperties) {
         super(properties)
@@ -27,11 +29,26 @@ export default class PunishmentAction extends Action implements PunishmentProper
     }
 
     formatError(message: string): string {
-        return
+        const targetTag = chalk.blueBright(this.target.user.tag)
+        const targetID = chalk.blueBright(this.target.user.id)
+        return `Could not ${this.type} ${targetTag} (${targetID}): ${message}`
     }
 
     formatSuccess(): string {
-        return
+        const pastAction = { mute: "Muted", kick: "Kicked", ban: "Banned" }[this.type]
+        const targetTag = chalk.blueBright(this.target.user.tag)
+        const targetID = chalk.blueBright(this.target.user.id)
+        const formattedLength = this.formatLength()
+        return `${pastAction} ${targetTag} (${targetID})${formattedLength}.`
+    }
+
+    formatLength(): string {
+        if (this.length === Infinity)
+            // prettier-ignore
+            return " permanently"
+        else if (this.length != null)
+            return ` for ${(this.length / 1000).toLocaleString()} seconds`
+        return ""
     }
 }
 
