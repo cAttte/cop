2262a7b24bbe87518533bb9a11830165ec47642d
cAttte <26514199+cAttte@users.noreply.github.com>
2020-11-16 00:26:05 -0300

    refactor(emptyMessages): move character list to separate dataset

--- src/modules/emptyMessages.ts
+++ src/modules/emptyMessages.ts
@@ -7,6 +7,7 @@ import Client from "../struct/Client"
 import Action from "../struct/action/Action"
 import DeleteAction from "../struct/action/DeleteAction"
 import PunishmentAction, { PunishmentProperties } from "../struct/action/PunishmentAction"
+import characters from "../data/blankCharacters"
 
 export default new Module({
     configSchema: {
@@ -94,37 +95,3 @@ function astToText(content: Token[], type?: string): string {
     }
     return result
 }
-
-const characters = [
-    "\u0020", // Space
-    "\u00A0", // No-Break Space
-    "\u17B5", // Khmer Vowel Inherent Aa
-    "\u2000", // En Quad
-    "\u2001", // Em Quad
-    "\u2002", // En Space
-    "\u2003", // Em Space
-    "\u2004", // Three-Per-Em Space
-    "\u2005", // Four-Per-Em Space
-    "\u2006", // Six-Per-Em Space
-    "\u2007", // Figure Space
-    "\u2008", // Punctuation Space
-    "\u2009", // Thin Space
-    "\u200A", // Hair Space
-    "\u200B", // Zero Width Space
-    "\u200C", // Zero Width Non-Joiner
-    "\u200D", // Zero Width Joiner
-    "\u200E", // Left-to-Right Mark
-    "\u200F", // Right-to-Left Mark
-    "\u2028", // Line Separator
-    "\u2029", // Paragraph Separator
-    "\u202F", // Narrow No-Break Space
-    "\u205F", // Medium Mathematical Space
-    "\u2800", // Braille Pattern Blank
-    "\u3000", // Ideographic Space
-    "\u3164", // Hangul Filler
-    "\uFEFF", // Zero Width No-Break Space
-    "\r", // Carriage Return
-    "\n", // Newline
-    "\t", // Tab
-    "\f" // Form Feed
-]
