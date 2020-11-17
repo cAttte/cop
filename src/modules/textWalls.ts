443b99b734d357f24a383ca0169159ce7428bd8d
cAttte <26514199+cAttte@users.noreply.github.com>
2020-11-17 06:57:25 -0300

    refactor(textWalls): use createMessageMatcher()

--- src/modules/textWalls.ts
+++ src/modules/textWalls.ts
@@ -5,9 +5,8 @@ import boolean from "../schema/boolean"
 import punishment from "../schema/punishment"
 import Module from "../struct/Module"
 import Client from "../struct/Client"
-import Action from "../struct/action/Action"
-import DeleteAction from "../struct/action/DeleteAction"
-import PunishmentAction, { PunishmentProperties } from "../struct/action/PunishmentAction"
+import { PunishmentProperties } from "../struct/action/PunishmentAction"
+import createMessageMatcher from "../util/createMessageMatcher"
 
 export default new Module({
     configSchema: {
@@ -15,60 +14,29 @@ export default new Module({
         delete: boolean.default(true),
         punishment: punishment
     },
-    events: {
-        message: createMessageHandler("message"),
-        messageUpdate: createMessageHandler("messageUpdate")
-    }
+    events: createMessageMatcher({
+        module: "TextWalls",
+        reason: "Text wall",
+        async matcher(
+            this: Client,
+            config: {
+                maxLines: number
+                delete: boolean
+                punishment: PunishmentProperties[]
+            },
+            message: Discord.Message
+        ) {
+            // 1,200 is a semi-arbitrary number of pixels that roughly
+            // equates to a single line in a Discord message.
+            // this of course depends on each user but it's accurate enough, i think!
+            let lines = 0
+            for (const line of message.content.split("\n")) {
+                const lineWidth = width(line, { size: 16 })
+                const remainder = lineWidth % 1200
+                const wrapValue = (lineWidth - remainder) / 1200
+                lines += wrapValue + 1
+            }
+            return lines <= config.maxLines
+        }
+    })
 })
-
-function createMessageHandler(event: "message" | "messageUpdate") {
-    return async function (
-        this: Client,
-        config: {
-            maxLines: number
-            delete: boolean
-            punishment: PunishmentProperties[]
-        },
-        oldMessage: Discord.Message,
-        newMessage?: Discord.Message
-    ): Promise<Action[]> {
-        const message = event === "message" ? oldMessage : newMessage
-        if (event === "messageUpdate" && oldMessage.content === newMessage.content) return
-        if (!message.content) return
-
-        // 1,200 is a semi-arbitrary number of pixels that roughly
-        // equates to a single line in a Discord message.
-        // this of course depends on each user but it's accurate enough, i think!
-        let lines = 0
-        for (const line of message.content.split("\n")) {
-            const lineWidth = width(line, { size: 16 })
-            const remainder = lineWidth % 1200
-            const wrapValue = (lineWidth - remainder) / 1200
-            lines += wrapValue + 1
-        }
-        if (lines <= config.maxLines) return
-
-        const actions: Action[] = []
-        if (config.delete) {
-            actions.push(
-                new DeleteAction({
-                    module: "TextWalls",
-                    target: message,
-                    reason: "Text wall"
-                })
-            )
-        }
-
-        if (config.punishment) {
-            actions.push(
-                PunishmentAction.processPunishment(config.punishment, {
-                    module: "TextWalls",
-                    target: message.member,
-                    reason: "Text wall"
-                })
-            )
-        }
-
-        return actions
-    }
-}
