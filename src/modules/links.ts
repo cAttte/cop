10c84387e360444c177463f0cc033d81a198dc8f
cAttte <26514199+cAttte@users.noreply.github.com>
2020-11-17 06:55:12 -0300

    refactor(links): use createMessageMatcher()

--- src/modules/links.ts
+++ src/modules/links.ts
@@ -4,9 +4,8 @@ import Discord from "discord.js"
 import Client from "../struct/Client"
 import boolean from "../schema/boolean"
 import Module from "../struct/Module"
-import Action from "../struct/action/Action"
-import DeleteAction from "../struct/action/DeleteAction"
-import PunishmentAction, { PunishmentProperties } from "../struct/action/PunishmentAction"
+import { PunishmentProperties } from "../struct/action/PunishmentAction"
+import createMessageMatcher from "../util/createMessageMatcher"
 import tlds from "../data/topLevelDomains"
 
 export default new Module({
@@ -15,76 +14,47 @@ export default new Module({
         validationLimit: Joi.number().integer().allow(Infinity).default(Infinity),
         delete: boolean
     },
-    events: {
-        message: createMessageHandler("message"),
-        messageUpdate: createMessageHandler("messageUpdate")
-    }
-})
-
-function createMessageHandler(event: "message" | "messageUpdate") {
-    return async function (
-        this: Client,
-        config: {
-            validate: boolean
-            validationLimit: number
-            delete: boolean
-            punishment: PunishmentProperties[]
-        },
-        oldMessage: Discord.Message,
-        newMessage?: Discord.Message
-    ): Promise<Action[]> {
-        const message = event === "message" ? oldMessage : newMessage
-        if (event === "messageUpdate" && oldMessage.content === newMessage.content) return
-        if (!message.content) return
-
-        // modified version of https://stackoverflow.com/a/3809435
-        const urlRegex = /(https?:\/\/)?(www\.)?([a-zA-Z0-9-._]{1,256})(\.[-a-zA-Z0-9]{2,6}){1,2}\b(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?/gi
-        let links: string[] = []
-        let matches: RegExpExecArray
-        while ((matches = urlRegex.exec(message.content))) links.push(matches[1])
-        if (config.validate) {
-            links = links.filter(url => {
-                const tld = url.match(/\.[a-z0-9]{2,6}$/i)?.[0]
-                if (!tld) return false
-                else if (tlds.includes(tld.toLowerCase())) return true
-                else return false
-            })
-        }
-        if (links.length === 0) return
-
-        if (config.validate && links.length <= config.validationLimit) {
-            let valid = false
-            for (const link of links) {
-                const fetched = await fetch(link).catch(() => null)
-                if (fetched) {
-                    valid = true
-                    break
-                }
+    events: createMessageMatcher({
+        module: "Links",
+        reason: "Posted link",
+        async matcher(
+            this: Client,
+            config: {
+                validate: boolean
+                validationLimit: number
+                delete: boolean
+                punishment: PunishmentProperties[]
+            },
+            message: Discord.Message
+        ) {
+            // modified version of https://stackoverflow.com/a/3809435
+            const urlRegex = /(https?:\/\/)?(www\.)?([a-zA-Z0-9-._]{1,256})(\.[-a-zA-Z0-9]{2,6}){1,2}\b(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?/gi
+            let links: string[] = []
+            let matches: RegExpExecArray
+            while ((matches = urlRegex.exec(message.content))) links.push(matches[1])
+            if (config.validate) {
+                links = links.filter(url => {
+                    const tld = url.match(/\.[a-z0-9]{2,6}$/i)?.[0]
+                    if (!tld) return false
+                    else if (tlds.includes(tld.toLowerCase())) return true
+                    else return false
+                })
             }
-            if (!valid) return
-        }
+            if (links.length === 0) return false
 
-        const actions: Action[] = []
-        if (config.delete) {
-            actions.push(
-                new DeleteAction({
-                    module: "Links",
-                    target: message,
-                    reason: "Posted link" + (links.length > 1 ? "s" : "")
-                })
-            )
-        }
+            if (config.validate && links.length <= config.validationLimit) {
+                let valid = false
+                for (const link of links) {
+                    const fetched = await fetch(link).catch(() => null)
+                    if (fetched) {
+                        valid = true
+                        break
+                    }
+                }
+                if (!valid) return false
+            }
 
-        if (config.punishment) {
-            actions.push(
-                PunishmentAction.processPunishment(config.punishment, {
-                    module: "Links",
-                    target: message.member,
-                    reason: "Posted link" + (links.length > 1 ? "s" : "")
-                })
-            )
+            return true
         }
-
-        return actions
-    }
-}
+    })
+})
