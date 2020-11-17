502d248f21bc3c41b73473c6368d56bcd8422731
cAttte <26514199+cAttte@users.noreply.github.com>
2020-11-17 02:31:02 -0300

    fix(emojis): type config parameter

--- src/modules/emojis.ts
+++ src/modules/emojis.ts
@@ -4,6 +4,7 @@ import punishment from "../schema/punishment"
 import * as Twemoji from "twemoji-parser"
 import Discord from "discord.js"
 import Module from "../struct/Module"
+import { PunishmentProperties } from "../struct/action/PunishmentAction"
 import createMessageMatcher from "../util/createMessageMatcher"
 
 export default new Module({
@@ -13,15 +14,22 @@ export default new Module({
         punishment: punishment
     },
     events: createMessageMatcher({
-        matcher(config: any, message: Discord.Message) {
+        module: "Emojis",
+        reason: "Emoji spam",
+        matcher(
+            config: {
+                limit: number
+                delete: boolean
+                punishment: PunishmentProperties[]
+            },
+            message: Discord.Message
+        ) {
             const unicodeEmoji = Twemoji.parse(message.content)
             const discordEmoji = message.content.match(/<a?:[_a-zA-Z0-9]{2,32}:\d{18}>/g)
             const emojiCount = unicodeEmoji.length + (discordEmoji?.length || 0)
 
             if (emojiCount > config.limit) return true
             else return false
-        },
-        module: "Emojis",
-        reason: "Emoji spam"
+        }
     })
 })
