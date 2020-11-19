6cbd31fc8b48a542595cc6b5bfd661a2912dbe69
cAttte <26514199+cAttte@users.noreply.github.com>
2020-11-19 06:53:33 -0300

    fix(schema): use PunishmentProvider in 'punishment' schema type

--- src/schema/punishment.ts
+++ src/schema/punishment.ts
@@ -1,8 +1,8 @@
 import Joi from "joi"
-import PunishmentAction from "../struct/action/PunishmentAction"
+import PunishmentProvider from "../struct/PunishmentProvider"
 
 export default Joi.string().custom(value => {
-    const result = PunishmentAction.parsePunishment(value)
+    const result = new PunishmentProvider(null).parsePunishment(value)
     if (result instanceof Error) throw result
     else return result
 })
