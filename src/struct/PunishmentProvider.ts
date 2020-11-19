34f26717e55cd6d8965bfade7df44df73ab7628e
cAttte <26514199+cAttte@users.noreply.github.com>
2020-11-19 02:24:11 -0300

    refactor(PunishmentAction)!: move parsePunishment() and processPunishment() to PunishmentProvider

--- src/struct/PunishmentProvider.ts
+++ src/struct/PunishmentProvider.ts
@@ -1,5 +1,9 @@
+import parseDuration from "parse-duration"
+import chalk from "chalk"
 import Discord from "discord.js"
 import Client from "./Client"
+import { ActionProperties } from "./action/Action"
+import PunishmentAction, { PunishmentProperties } from "./action/PunishmentAction"
 
 export default class PunishmentProvider {
     client: Client
@@ -19,4 +23,72 @@ export default class PunishmentProvider {
     async ban(member: Discord.GuildMember, reason: string, length: number) {
         await member.ban()
     }
+
+    static parsePunishment(input: string | string[]): PunishmentProperties[] | Error {
+        if (typeof input === "string")
+            input = input.split(/ +?(,|;|then|(,|;) +?then) +?/i)
+
+        const parsed: PunishmentProperties[] = []
+        for (const punishment of input) {
+            if (punishment.match(/null|none|nothing/i)) {
+                parsed.push({ type: "null" })
+                continue
+            }
+
+            const properties: PunishmentProperties = { type: null }
+            const typeMatch = punishment.match(/^(mute|kick|ban) +?/i)
+            const type = (typeMatch[1] || "").toLowerCase()
+            if (!["mute", "kick", "ban"].includes(type))
+                return new Error(
+                    `Could not interpret punishment "${chalk.redBright(punishment)}".`
+                )
+            properties.type = <"mute" | "kick" | "ban">type
+
+            const rawLength = punishment
+                .slice(typeMatch[0].length)
+                .trim()
+                .replace(/^for +?/i, "")
+            if (rawLength) {
+                if (type === "kick")
+                    return new Error(`Punishment type "kick" does not accept a length.`)
+
+                const length = permanent.includes(rawLength)
+                    ? Infinity
+                    : parseDuration(rawLength)
+                if (!length)
+                    return new Error(
+                        `Could not interpret length ${chalk.redBright(rawLength)}.`
+                    )
+                properties.length = length
+            } else if (!rawLength && type !== "kick")
+                return new Error(
+                    `Missing punishment length in ${chalk.redBright(punishment)}.`
+                )
+
+            parsed.push(properties)
+        }
+
+        return parsed
+    }
+
+    // return first punishment for now, todo: check punishment history
+    static processPunishment(
+        client: Client,
+        punishments: PunishmentProperties[],
+        properties: ActionProperties
+    ): PunishmentAction {
+        return new PunishmentAction(client, { ...punishments[0], ...properties })
+    }
 }
+
+const permanent = [
+    "forever",
+    "ever",
+    "permanent",
+    "permanently",
+    "infinity",
+    "infinite",
+    "inf",
+    "∞",
+    "until the end of times"
+]
--- src/struct/action/PunishmentAction.ts
+++ src/struct/action/PunishmentAction.ts
@@ -1,7 +1,6 @@
 import chalk from "chalk"
 import Discord from "discord.js"
 import Client from "../Client"
-import parseDuration from "parse-duration"
 import Action, { ActionProperties } from "./Action"
 
 type PunishmentType = "null" | "mute" | "kick" | "ban"
@@ -19,62 +18,6 @@ export default class PunishmentAction extends Action {
         super(client, properties)
         this.type = properties.type
         this.detail = properties.detail == null ? properties.length : properties.detail
-    }
-
-    static parsePunishment(input: string | string[]): PunishmentProperties[] | Error {
-        if (typeof input === "string")
-            input = input.split(/ +?(,|;|then|(,|;) +?then) +?/i)
-
-        const parsed: PunishmentProperties[] = []
-        for (const punishment of input) {
-            if (punishment.match(/null|none|nothing/i)) {
-                parsed.push({ type: "null" })
-                continue
-            }
-
-            const properties: PunishmentProperties = { type: null }
-            const typeMatch = punishment.match(/^(mute|kick|ban) +?/i)
-            const type = (typeMatch[1] || "").toLowerCase()
-            if (!["mute", "kick", "ban"].includes(type))
-                return new Error(
-                    `Could not interpret punishment "${chalk.redBright(punishment)}".`
-                )
-            properties.type = <"mute" | "kick" | "ban">type
-
-            const rawLength = punishment
-                .slice(typeMatch[0].length)
-                .trim()
-                .replace(/^for +?/i, "")
-            if (rawLength) {
-                if (type === "kick")
-                    return new Error(`Punishment type "kick" does not accept a length.`)
-
-                const length = permanent.includes(rawLength)
-                    ? Infinity
-                    : parseDuration(rawLength)
-                if (!length)
-                    return new Error(
-                        `Could not interpret length ${chalk.redBright(rawLength)}.`
-                    )
-                properties.length = length
-            } else if (!rawLength && type !== "kick")
-                return new Error(
-                    `Missing punishment length in ${chalk.redBright(punishment)}.`
-                )
-
-            parsed.push(properties)
-        }
-
-        return parsed
-    }
-
-    // return first punishment for now, todo: check punishment history
-    static processPunishment(
-        client: Client,
-        punishments: PunishmentProperties[],
-        properties: ActionProperties
-    ): PunishmentAction {
-        return new PunishmentAction(client, { ...punishments[0], ...properties })
     }
 
     async execute(muteRole: Discord.Role): Promise<Discord.Base | Error> {
@@ -123,15 +66,3 @@ export type PunishmentProperties = {
     type: "null" | "mute" | "kick" | "ban"
     length?: number
 }
-
-const permanent = [
-    "forever",
-    "ever",
-    "permanent",
-    "permanently",
-    "infinity",
-    "infinite",
-    "inf",
-    "∞",
-    "until the end of times"
-]
