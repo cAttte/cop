6532db2e2604fc29a1b2f9c4365b75074086f98a
cAttte <26514199+cAttte@users.noreply.github.com>
2020-11-20 10:38:37 -0300

    feat(Client): create 'db' property

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -1,10 +1,19 @@
+import chalk from "chalk"
 import Discord from "discord.js"
+import Sequelize from "sequelize"
 import logger from "../logger"
 import PunishmentProvider from "./PunishmentProvider"
 
 type Config = { muteRole?: Discord.Role; modules: any }
 
+const sequelize = new Sequelize.Sequelize({
+    dialect: "sqlite",
+    storage: __dirname + `/../../db/cop.sqlite`,
+    logging: msg => logger.debug(`${chalk.gray("[Sequelize]")} ${msg}`)
+})
+
 export default class Client extends Discord.Client {
+    db = sequelize
     logger = logger
     punishment = new PunishmentProvider(this)
     config: Config
